e6b50de75715f8817a36a82d84562ae99b28f66e
xyy <1620776077@qq.com>
2020-06-25 00:21:52 +0800

    修改了一些小BUG

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from '../components/user/Users.vue'
-import Rights from '../components/power/Rights.vue'
-import Roles from '../components/power/Rules.vue'
-import Cate from '../components/goods/Cate.vue'
-import Params from '../components/goods/Params.vue'
-import List from '../components/goods/List.vue'
-import Add from '../components/goods/Add.vue'
-import Order from '../components/order/Order.vue'
-import Report from '../components/report/Report.vue'
+// 路由懒加载
+// import Login from '../components/Login.vue'
+const Login = () => import('../components/Login.vue')
+// import Home from '../components/Home.vue'
+const Home = () => import('../components/Home.vue')
+// import Welcome from '../components/Welcome.vue'
+const Welcome = () => import('../components/Welcome.vue')
+
+// import Users from '../components/user/Users.vue'
+// import Rights from '../components/power/Rights.vue'
+// import Roles from '../components/power/Rules.vue'
+const Users = () => import('../components/user/Users.vue')
+const Rights = () => import('../components/power/Rights.vue')
+const Roles = () => import('../components/power/Roles.vue')
+
+// import Cate from '../components/goods/Cate.vue'
+// import Params from '../components/goods/Params.vue'
+const Cate = () => import('../components/goods/Cate.vue')
+const Params = () => import('../components/goods/Params.vue')
+
+// import List from '../components/goods/List.vue'
+// import Add from '../components/goods/Add.vue'
+const List = () => import('../components/goods/List.vue')
+const Add = () => import('../components/goods/Add.vue')
+
+// import Order from '../components/order/Order.vue'
+// import Report from '../components/report/Report.vue'
+const Order = () => import('../components/order/Order.vue')
+const Report = () => import('../components/report/Report.vue')
 
 Vue.use(VueRouter)
 
